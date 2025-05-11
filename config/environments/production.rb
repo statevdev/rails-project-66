f2086cae4814110de9b7d2342ba6be2dfef19ac7
statevdev <statev.dev@gmail.com>
2025-05-12 03:20:48 +0600

    try fix production errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
+  config.default_url_options = { host: 'rails-project-66-l5zc.onrender.com' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
