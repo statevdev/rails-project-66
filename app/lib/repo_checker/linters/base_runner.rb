2d44d1114d85d4bff27d3d5bf9549c69fc6d4855
statevdev <statev.dev@gmail.com>
2025-05-18 23:47:35 +0600

    debugging with logger 3

--- app/lib/repo_checker/linters/base_runner.rb
+++ app/lib/repo_checker/linters/base_runner.rb
@@ -13,11 +13,14 @@ class RepoChecker::Linters::BaseRunner
 
   def run(command)
     cmd = "#{command} #{@target_dir}"
+
+    Rails.logger.info("DEBUG -- CMD running: #{cmd}")
     stdout, status = Open3.popen3(cmd) { |_stdin, stdout, _stderr, wait_thr| [stdout.read, wait_thr.value] }
 
     return @result if status.success?
 
     @result = JSON.parse(stdout)
+    Rails.logger.info("DEBUG -- RESULT: #{@result.inspect}")
   ensure
     FileUtils.rm_rf(@target_dir)
   end
